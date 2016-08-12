31ed18b042735958e370fb2a2c4db3a62f16b343
Elizabeth <elizabethsong65@yahoo.com>
2016-08-12 15:11:36 -0400

    add configurations for postgres DB for herku

--- php/index.php
+++ php/index.php
@@ -1,5 +1,27 @@
 <?php
+	$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+   array(
+   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+   'pdo.username' => $dbopts["user"],
+   'pdo.password' => $dbopts["pass"]
+   )
+);
 
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
+});
 ?>
 
 <?php include("auth.php"); //include auth.php file on all secure pages ?>
