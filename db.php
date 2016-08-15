1b57a0aa9e36a84334f69e332c0241f81a1bc131
Elizabeth <elizabethsong65@yahoo.com>
2016-08-14 23:17:44 -0400

    modified db.php to add database

--- db.php
+++ db.php
@@ -1,10 +1,15 @@
-<?php
-?>
 
 
 <?php
- 
-$connection = mysqli_connect('localhost', 'esong', 'Es100601%', 'register');
+
+ $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"],1);
+  
+$connection = mysqli_connect($server, $username, $password, $db);
 //$connection = mysqli_connect('localhost', 'root', '', 'register');
 
 /* check connection */
@@ -12,6 +17,10 @@ if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
+
+
+
+
 
 // Change database to "register" if needed
 //mysqli_select_db($connection,'register');
